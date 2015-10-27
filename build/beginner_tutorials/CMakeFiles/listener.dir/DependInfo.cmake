d8b7caa9450b1e438587bc8fe995a64571ee157a
Juampe López <jplopez@circutor.com>
2015-10-27 13:47:26 +0100

    Building talker.cpp and listener.cpp.

--- build/beginner_tutorials/CMakeFiles/listener.dir/DependInfo.cmake
+++ build/beginner_tutorials/CMakeFiles/listener.dir/DependInfo.cmake
@@ -1,7 +1,12 @@
 # The set of languages for which implicit dependencies are needed:
 SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jplopez/catkin_ws/src/beginner_tutorials/src/listener.cpp" "/home/jplopez/catkin_ws/build/beginner_tutorials/CMakeFiles/listener.dir/src/listener.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 SET(CMAKE_TARGET_DEFINITIONS
